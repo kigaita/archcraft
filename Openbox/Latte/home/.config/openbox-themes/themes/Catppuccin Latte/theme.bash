d3c01af0a5463c4e95212d3f157d7124c01c1fc5
Vanillyn <skyykiwi@gmail.com>
2023-02-19 11:30:17 -0500

    update naming; 50% Latte

---
+++ Openbox/Latte/home/.config/openbox-themes/themes/Catppuccin Latte/theme.bash
@@ -0,0 +1,92 @@
+# ------------------------------------------------------------------------------
+# Made with love by Lamp & Vanillyn :)
+#
+#  Catppuccin Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#eff1f5'
+foreground='#4c4f69'
+color0='#e6e9ef'
+color1='#d20f39'
+color2='#40a02b'
+color3='#df8e1d'
+color4='#1e66f5'
+color5='#8839ef'
+color6='#179299'
+color7='#4c4f69'
+color8='#e6e9ef'
+color9='#d20f39'
+color10='#40a02b'
+color11='#df8e1d'
+color12='#1e66f5'
+color13='#8839ef'
+color14='#179299'
+color15='#4c4f69'
+
+accent='#8087a2'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='JetBrains Mono:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Zafiro'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='manhattan.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='catppuccin'
+icon_theme='Catppuccin'
+cursor_theme='Qogirr-Dark'
+
+# Openbox
+ob_theme='catppuccin'
+ob_layout='NLIMC'
+ob_font='JetBrains Mono'
+ob_font_size='9'
+ob_menu='menu-icons.xml'
+ob_margin_t='78'
+ob_margin_b='20'
+ob_margin_l='20'
+ob_margin_r='20'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='20x78'
+dunst_origin='top-right'
+dunst_font='JetBrains Mono 10'
+dunst_border='0'
+dunst_separator='2'
+
+# Plank
+plank_hmode='intelligent'
+plank_offset='0'
+plank_position='bottom'
+plank_theme='Transparent'
+plank_icon_size='32'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='12'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
