1e2cb2d46b4c52396e1fc2bd2bf11d378e70bea8
Vanillyn <skyykiwi@gmail.com>
2023-02-21 16:46:35 -0500

    Clear .config folders for unnecessary configs

--- openbox/scripts/archcraft-files.sh
+++ openbox/scripts/archcraft-files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo Installing necessary programs
-sudo pacman -S --noconfirm openbox xorg qt5ct pastel nitrogen polybar rofi mpd xmlstarlet dunst lxappearance obconf
+sudo pacman -S --noconfirm openbox xorg qt5ct pastel nemo nitrogen polybar rofi mpd xmlstarlet dunst lxappearance obconf xsettingsd
 echo Copying files
 sudo cp -rv ../Archcraft\ Files/archcraft /usr/share/
 echo Finished installing necessary Archcraft files
