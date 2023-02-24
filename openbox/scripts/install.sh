405ace5a5a3e9dfc7165cb70d99ae9d0cd1c2577
Vanillyn <skyykiwi@gmail.com>
2023-02-23 21:03:44 -0500

    organize

--- openbox/scripts/install.sh
+++ openbox/scripts/install.sh
@@ -3,9 +3,8 @@
 # Archcraft Catppuccin installation script
 # vanillyn, nekowinston
 
-# GTK Installation
-
-echo "Select your flavor"
+# Flavor Selection
+echo "Select your flavor!"
 select fl in "Mocha" "Frappe" "Macchiato" "Latte"; do
     case "$fl" in
         "Mocha") FLAVOR="Mocha"; VALUE="Dark";
@@ -17,19 +16,18 @@ select fl in "Mocha" "Frappe" "Macchiato" "Latte"; do
         "Latte") FLAVOR="Latte"; VALUE="Light";
             ;;
     esac
-
     THEME="Catppuccin-$FLAVOR"
 
     echo "x------ Copying files ------x"
-     cp -rv "../$FLAVOR/home/.config" ~
-     cp "../$FLAVOR/home/Wallpapers" ~/Pictures
+     cp -rv "../$FLAVOR/.config" ~
+     cp "../$FLAVOR/Wallpapers" ~/Pictures
 
     echo "x------ Installing GTK theme ------x"
         git clone https://github.com/catppuccin/gtk tmp
 
-        cd tmp || exii
+        cd tmp || exit
         pip install catppuccin
-        
+
         gtkdir=$(pwd)/tmp
         python3 ./install.py "${FLAVOR,,}" --accent pink --dest "${gtkdir}/dist"
         cp -r "dist/Catppuccin-$FLAVOR-Standard-Pink-$VALUE" "dist/Catppuccin-$FLAVOR"
@@ -42,5 +40,4 @@ select fl in "Mocha" "Frappe" "Macchiato" "Latte"; do
     echo "x------ Running start script ------x"
         ~/.config/openbox-themes/themes/$THEME/apply.sh
     break
-
 done
